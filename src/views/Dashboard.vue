09f43f39622b5ac871b20123d4be8efdfb1841d7
André Monello <atmonello@outlook.com>
2020-05-02 14:34:27 -0300

    Removing uneeded boilerplate components

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+    <dashboard-header />
+  </div>
+</template>
+
+<script>
+import DashboardHeader from "@/components/Header.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    DashboardHeader,
+  },
+};
+</script>
