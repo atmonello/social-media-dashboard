81e6b06dd9cf192b69813e01c840f76deb375316
André Monello <atmonello@outlook.com>
2020-05-02 14:36:10 -0300

    Adding header and toggle component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="dashboard__header">
+    <div class="dashboard__header__text">
+      <h1>Social Media Dashboard</h1>
+      <small>Total Followers: 23.004</small>
+    </div>
+    <div class="dashboard__header__toggle">
+      <dashboard-toggle />
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+.dashboard__header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
+<script>
+import DashboardToggle from "@/components/Toggle.vue";
+
+export default {
+  components: {
+    DashboardToggle,
+  },
+};
+</script>
---
+++ src/components/Toggle.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="dashboard__toggle">
+    <span>Dark Mode</span>
+    <div class="dashboard__toggle__container">
+      <input type="radio" v-model="toggle" />
+      <div class="toggle" :class="{'on': toggle}" @click="setToggle()">
+        <span class="toggle__icon"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Mutation } from "vuex-class";
+
+@Component({
+  name: "DashboardToggle",
+})
+export default class DashboardToggle extends Vue {
+  @Mutation("setTheme") setTheme;
+
+  private toggle = false;
+
+  public setToggle() {
+    this.toggle = !this.toggle;
+    const theme = this.toggle ? "dark" : "light";
+    this.setTheme(theme);
+  }
+}
+</script>
+
+<style lang="scss">
+.dashboard__toggle {
+  display: flex;
+  align-items: center;
+  padding-right: 64px;
+
+  &__container {
+    position: relative;
+    margin-left: 16px;
+
+    .toggle {
+      cursor: pointer;
+      position: absolute;
+      z-index: 1;
+      height: 32px;
+      width: 64px;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      margin: auto;
+      border-radius: 20px;
+      background-color: blue;
+      box-sizing: border-box;
+      padding: 4px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+
+      &__icon {
+        border-radius: 100%;
+        border: 1px solid #fff;
+        height: 100%;
+        width: 24px;
+        background-color: #fff;
+      }
+
+      &.on {
+        background: red;
+        justify-content: flex-end;
+      }
+    }
+  }
+}
+</style>
